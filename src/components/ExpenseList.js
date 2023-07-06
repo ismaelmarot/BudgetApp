bea568a9f45fb729e090f912899d7bddb434dd18
ismaelmarot <ismaelmarot@hotmail.com>
2023-07-06 23:41:21 +1000

    ExpenseItem & ExpenseList components added

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+const ExpenseList = () =>{
+    const expenses = [
+        { id: 123, name: 'shoping', cost: 200 },
+        { id: 456, name:'rent', cost: 300 },
+        { id: 789, name: 'food', cost: 250 },
+        { id: 101, name: 'transportation', cost: 50 },
+    ]
+
+    return (
+        <ul>
+            {expenses.map((expense)=>(
+                <ExpenseItem 
+                    key={ expense.id }
+                    id={ expense.id }
+                    name={ expense.name }
+                    cost={ expense.cost }
+                />
+            ))}    
+        </ul>
+    );
+}
+
+export default ExpenseList;
